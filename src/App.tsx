d5f39373bf48a3b86df5af49b5cbc5b6c1cdd351
Mateus Barreto <mateusoliveirabarreto2@gmail.com>
2025-06-10 16:33:17 -0300

    fix: container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ThemeProvider, CssBaseline, Box, Container } from '@mui/material';
+import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import theme from './theme';
 import Header from './components/Header';
 import Footer from './components/Footer';
